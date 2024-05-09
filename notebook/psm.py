dca3a7a2e239cf3ec272b06c7160078b76ff0764
xiaolizhang <xiaolizhang@tencent.com>
2024-05-09 09:47:50 +0800

    add effect size and compile for online mode

--- notebook/psm.py
+++ notebook/psm.py
@@ -1,6 +1,7 @@
 # -*-coding:utf-8 -*-
 # @Author: xiaolizhang
 
+import pandas as pd
 import numpy as np
 import lightgbm as lgb
 
@@ -27,25 +28,30 @@ class OptimPsmPy(PsmPy):
     Support multiple fit model for psm
     """
 
-    def __init__(self, data, treatment, indx, exclude, raw_data):
-        super(OptimPsmPy, self).__init__(data, treatment, indx, exclude, raw_data)
+    def __init__(self, data, treatment, indx, exclude, train_data, control_data):
+        super(OptimPsmPy, self).__init__(data, treatment, indx, exclude, train_data, control_data)
         self.category_columns = exclude
         self.model = None
-        self.raw_data = raw_data
+        self.train_data = train_data
+        self.control_data = control_data
+        self.control_data[treatment] = 0
 
-    def lightgbm_ps(self):
+    def lightgbm_ps(self, num_boost_round=100):
         # preprocessing data
-        columns = self.raw_data.columns.tolist()
+        columns = self.train_data.columns.tolist()
         columns.remove(self.treatment)
         columns.remove(self.indx)
         # print('feature columns >>\n{}'.format(',\n'.join(columns)))
         # columns.remove(self.exclude)
 
-        idx, x, y = self.raw_data[self.indx], self.raw_data[columns], self.raw_data[self.treatment]
+        print('split training data ...')
+        idx, x, y = self.train_data[self.indx], self.train_data[columns], self.train_data[self.treatment]
         x_train, x_test, y_train, y_test = train_test_split(x, y)
         train_data = lgb.Dataset(x_train, y_train, categorical_feature=self.category_columns)
         val_data = lgb.Dataset(x_test, y_test, categorical_feature=self.category_columns)
+
         # build model
+        print('training model ...')
         params = {
             'num_leaves': 32,
             'max_depth': 8,
@@ -65,36 +71,51 @@ class OptimPsmPy(PsmPy):
         }
         clf = lgb.train(params=params,
                         train_set=train_data,
-                        num_boost_round=50,
+                        num_boost_round=num_boost_round,
                         valid_sets=[train_data, val_data])
         self.model = clf
 
+        # evaluate data
+        self.evaluate_data = x
+        self.evaluate_data['propensity_score'] = clf.predict(x)
+        self.evaluate_data['propensity_logit'] = self.evaluate_data['propensity_score'].apply(
+            lambda p: np.log(p / (1 - p)) if p < 0.9999 else np.log(p / (0.00001)))
+        self.evaluate_data[self.indx] = idx
+        self.evaluate_data[self.treatment] = y
+
+        treatment_data = self.train_data[self.train_data[self.treatment] == 1]
+        idx_treatment, x_treatment, y_treatment = treatment_data[self.indx], treatment_data[columns], treatment_data[self.treatment]
+        idx_control, x_control, y_control = self.control_data[self.indx], self.control_data[columns], self.control_data[self.treatment]
         # predict and merge
-        self.predicted_data = x
-        self.predicted_data['propensity_score'] = clf.predict(x)
+        self.predicted_data = pd.concat([x_treatment, x_control], ignore_index=True)
+        self.predicted_data['propensity_score'] = clf.predict(self.predicted_data )
         self.predicted_data['propensity_logit'] = self.predicted_data['propensity_score'].apply(
             lambda p: np.log(p / (1 - p)) if p < 0.9999 else np.log(p / (0.00001)))
-        self.predicted_data[self.indx] = idx
-        self.predicted_data[self.treatment] = y
+        self.predicted_data[self.indx] = pd.concat([idx_treatment, idx_control], ignore_index=True)
+        self.predicted_data[self.treatment] = pd.concat([y_treatment, y_control], ignore_index=True)
 
 
 class PropensityScoreMatching:
 
-    def __init__(self, label_column, index_column, fc_columns, dataset, category_fc_columns=[], model_type='logit'):
+    def __init__(self, label_column, index_column, fc_columns, dataset, control_data, model_config,
+                 category_fc_columns=[]):
         self.index_column = index_column
         self.label_column = label_column
         self.fc_columns = fc_columns
         self.category_columns = category_fc_columns
         used_columns = fc_columns + [index_column, label_column]
         self.data = dataset[used_columns]
-        self.model_type = model_type
+        self.control_data = control_data[used_columns]
+        self.model_config = model_config
+        self.model_type = model_config['model_type']
 
     def build(self):
         self._model = OptimPsmPy(data=self.data,
                                  treatment=self.label_column,
                                  indx=self.index_column,
                                  exclude=self.category_columns,
-                                 raw_data=self.data)
+                                 train_data=self.data,
+                                 control_data=self.control_data)
 
     def fit(self):
         # fit stage:
@@ -103,7 +124,7 @@ class PropensityScoreMatching:
             self._model.logistic_ps(balance=False)
         elif self.model_type == 'lgb':
             print('LightGBM model is training ... ')
-            self._model.lightgbm_ps()
+            self._model.lightgbm_ps(num_boost_round=self.model_config['num_boost_round'])
         # matching stage:
         print('KNN matching')
         self._model.knn_matched(matcher='propensity_logit', replacement=True, caliper=None)
@@ -117,7 +138,7 @@ class PropensityScoreMatching:
         df_matched = self._model.df_matched
         format_matched = df_matched.dropna()
         format_matched = format_matched[format_matched[self.index_column] != format_matched.matched_ID]
-        return format_matched
+        return self._model.predicted_data, format_matched
 
     def predict_propensity_score(self, predicted_data, saved_columns=[]):
         model = self._model.model
@@ -128,7 +149,7 @@ class PropensityScoreMatching:
         return predicted_data[default_saved_columns + saved_columns]
 
     def evaluate(self):
-        y_data = self._model.predicted_data[self.label_column]
-        y_pred = self._model.predicted_data['propensity_score'].apply(lambda x: 1 if x > 0.5 else 0)
+        y_data = self._model.evaluate_data[self.label_column]
+        y_pred = self._model.evaluate_data['propensity_score'].apply(lambda x: 1 if x > 0.5 else 0)
         print(classification_report(y_data, y_pred))
         print("metric AUC: {auc:.4f}".format(auc=cal_auc(y_data, y_pred)))
--- notebook/run.py
+++ notebook/run.py
@@ -3,9 +3,16 @@
 
 
 import yaml
+import math
 
 import pandas as pd
 pd.options.mode.chained_assignment = None
+import matplotlib
+matplotlib.use('TkAgg')
+import seaborn as sns
+sns.set(rc={'figure.figsize': (10, 8)}, font_scale=1.3)
+import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
 
 from notebook.psm import PropensityScoreMatching
 
@@ -76,12 +83,13 @@ def parse_schema(schema):
 
 def parse_category_feat(data, category_columns):
     feature = dict()
+    # print(category_columns)
     for col in category_columns:
         feature[col] = data[col].unique().tolist()
     return feature
 
 
-def print_log(data, treatment_col, label_col, return_outcome=False):
+def generate_distribution(data, treatment_col, label_col, return_outcome=False):
     data['binary_treatment'] = data[treatment_col].apply(lambda x: 1 if x > 0 else 0)
     sample_cnt = data.shape[0]
     positive_cnt = data['binary_treatment'].sum()
@@ -100,6 +108,70 @@ def print_log(data, treatment_col, label_col, return_outcome=False):
         return content
 
 
+def plot_effect_size(df_before, df_after, treatment, vars, save=False):
+    title = 'Standardized Mean differences accross covariates before and after matching'
+    before_color, after_color = '#FCB754', '#3EC8FB'
+
+    def cohenD(df, treatment, metricName):
+        treated_metric = df[df[treatment] == 1][metricName]
+        untreated_metric = df[df[treatment] == 0][metricName]
+        # calculate denominator
+        denominator = math.sqrt(
+            ((treated_metric.count() - 1) * treated_metric.std() ** 2 + (untreated_metric.count() - 1)
+             * untreated_metric.std() ** 2) / (treated_metric.count() + untreated_metric.count() - 2))
+        # if denominator is 0 divide by very small number
+        if denominator == 0:
+            d = abs((treated_metric.mean() - untreated_metric.mean()) / 0.000001)
+        else:
+            d = abs((treated_metric.mean() - untreated_metric.mean()) / denominator)
+        return d
+
+    data = []
+    for cl in vars:
+        if 'category' not in cl:
+            data.append([cl, 'before', cohenD(
+                df_before, treatment, cl)])
+            data.append([cl, 'after', cohenD(
+                df_after, treatment, cl)])
+    effect_size = pd.DataFrame(data, columns=['Variable', 'matching', 'Effect Size'])
+    sns.set_style("white")
+    sns_plot = sns.barplot(data=effect_size, y='Variable', x='Effect Size', hue='matching', palette=[
+        before_color, after_color], orient='h')
+    sns_plot.set(title=title)
+    if save:
+        sns_plot.figure.savefig(
+            'effect_size.png', dpi=250, bbox_inches="tight")
+    else:
+        pass
+
+
+def plot_match(df, treatment, Title, save_name='propensity_match.png', save=False):
+    # data process
+    dftreat = df[df[treatment] == 1]
+    dfcontrol = df[df[treatment] == 0]
+    matched_entity = 'propensity_logit'
+    x1 = dftreat[matched_entity]
+    x2 = dfcontrol[matched_entity]
+    # Assign colors for each airline and the names
+    colors = ['#E69F00', '#56B4E9']
+    names = ['treatment', 'control']
+    sns.set_style("white")
+    # Make the histogram using a list of lists
+    # Normalize the flights and assign colors and names
+    plt.hist([x1, x2], color=colors, label=names, density=True)
+    plt.plot(x1, density(xs))
+    # Plot formatting
+    plt.legend()
+    plt.xlabel('propensity logit')
+    plt.ylabel('density of users')
+    plt.title(label=Title)
+    if save:
+        plt.savefig(save_name, dpi=250)
+        plt.pause(0)
+    else:
+        pass
+
+
 def preprocessing_data(data, features):
     # preprocessing category columns
     for col, vocab in features.items():
@@ -107,9 +179,13 @@ def preprocessing_data(data, features):
     return data
 
 
-def main(file_path):
-    assert len(file_path) == 2, 'input file path should be 2'
-    schema_path, data_path = file_path[0], file_path[1]
+def main(input, model_config, dataset=None, offline=True):
+    file_path = input.split(",")
+    if offline:
+        assert len(file_path) == 2, 'input file path should be 2'
+        schema_path, data_path = file_path[0], file_path[1]
+    else:
+        schema_path = file_path[0]
 
     # config
     print("load yaml file {} ... ".format(schema_path))
@@ -117,50 +193,96 @@ def main(file_path):
     config = parse_schema(schema)
 
     # load data
-    raw_data = pd.read_csv(data_path, encoding='utf-8')
+    if offline:
+        raw_data = pd.read_csv(data_path, encoding='utf-8')
+    else:
+        raw_data = dataset
     raw_data.columns = list(schema.keys())
+    raw_data.fillna('未知', inplace=True)
+    raw_data[config['index']] = raw_data[config['index']].astype(str)
+    raw_data[config['numerical_fc']] = raw_data[config['numerical_fc']].apply(pd.to_numeric)
 
     treatment_col, exp_col, label_col = config['treatment'], config['exp'], config['label']
     # data distribution
     train_data = raw_data[raw_data[exp_col] == EXP_GROUP]
-    train_data.fillna('未知', inplace=True)
-    predicted_data = raw_data[raw_data[exp_col] == CONTROL_GROUP]
+    control_data = raw_data[raw_data[exp_col] == CONTROL_GROUP]
     print(">>>> Data distribution")
-    print("Train data >> \n{}".format(print_log(train_data, treatment_col, label_col)))
-    print("Predicted data >> \n{}".format(print_log(predicted_data, treatment_col, label_col)))
+    print("Train data >> \n{}".format(generate_distribution(train_data, treatment_col, label_col)))
+    print("Control data >> \n{}".format(generate_distribution(control_data, treatment_col, label_col)))
 
     # pearson correlation：only numerical feature
     corr_matrix = abs(train_data[[treatment_col] + config['numerical_fc']].corr())
     feat_corr = abs(corr_matrix[treatment_col]).sort_values(ascending=False)
-    print("\n>>>> Feature Selection")
-    print("Correlation with is treatment >> ")
-    print(feat_corr[1:11])
-    # feature selection
-    used_feat = feat_corr[1:20].index.to_list() + config['category_fc']
 
-    # preprocessing column
+    corr_y_matrix = abs(train_data[[label_col] + config['numerical_fc']].corr())
+    feat_corr_y = abs(corr_y_matrix[label_col]).sort_values(ascending=False).to_dict()
+
+    print("\n>>>> Feature Selection")
+    print("Selected features correlation with is treatment >> ")
+    # print(feat_corr[1:11])
+    # feature selection
+    used_feat = []
+    used_feat.extend(config['category_fc'])
+    filter_col = []
+    for k, v in feat_corr[1:].to_dict().items():
+        if k in filter_col:
+            continue
+
+        corr_y = feat_corr_y[k]
+        if v < 0.1 and corr_y < 0.1:
+            print("skipped feature {}".format(k))
+            filter_col.append(k)
+        else:
+            print("feature: {}, correlation with treatment: {c_t: .4f}, with outcome: {c_o: .4f}".format(k,
+                                                                                                         c_t=v,
+                                                                                                         c_o=corr_y))
+            used_feat.append(k)
+            # filter high correlation feature
+            cols = corr_matrix[(corr_matrix[k] > 0.6) & (corr_matrix[k] < 1)][k].index.to_list()
+            filter_col.extend(cols)
+    print("used feature number: {}".format(len(used_feat)))
+
+    # preprocessing data
     features = parse_category_feat(train_data, config['category_fc'])
     train_data = preprocessing_data(train_data, features)
+    control_data = preprocessing_data(control_data, features)
 
-    print("\n>>>> Model Training")
+    print("\n>>>> Build Model")
     # build model
     model = PropensityScoreMatching(label_column=treatment_col,
                                     index_column=config['index'],
                                     fc_columns=used_feat,
                                     dataset=train_data,
-                                    category_fc_columns=config['category_fc'],
-                                    model_type='lgb')
+                                    control_data=control_data,
+                                    model_config=model_config,
+                                    category_fc_columns=config['category_fc'])
     model.build()
     model.fit()
     # model evaluation
     model.evaluate()
 
-    # predict
-    predicted_data = preprocessing_data(predicted_data, features)
-    control_data = model.predict_propensity_score(predicted_data, saved_columns=[label_col])
-    exp_content, exp_outcome, exp_t_outcome = print_log(train_data, treatment_col, label_col, return_outcome=True)
-    control_content, control_outcome, control_t_outcome = print_log(control_data, 'binary_predicted_label', label_col,
+    # balanced test
+    df_treatment, df_before_control = train_data[train_data[treatment_col] == 1], control_data
+    df_before_control[treatment_col] = 0
+    df_before = pd.concat([df_treatment, df_before_control], ignore_index=True)
+    df_after, df_matched = model.predict()
+    pairs = df_matched.matched_ID.unique().tolist() + df_matched.user_id.unique().tolist()
+    plot_match(df_after, treatment_col, Title='Propensity Scores Before Matching',
+               save_name='propensity_match_before.png', save=True)
+    plot_match(df_after[df_after['user_id'].isin(pairs)], treatment_col, Title='Propensity Scores After Matching',
+               save_name='propensity_match_after.png', save=True)
+    plot_effect_size(df_before, df_after[df_after['user_id'].isin(pairs)],
+                     treatment=treatment_col,
+                     vars=used_feat,
+                     save=True)
+
+    # treatment effect
+    control_data = model.predict_propensity_score(control_data, saved_columns=[label_col] + used_feat)
+    exp_content, exp_outcome, exp_t_outcome = generate_distribution(train_data, treatment_col, label_col,
                                                                     return_outcome=True)
+    control_content, control_outcome, control_t_outcome = generate_distribution(control_data, 'binary_predicted_label',
+                                                                                label_col,
+                                                                                return_outcome=True)
     print("Exp data >> \n{}".format(exp_content))
     print("Control data >> \n{}".format(control_content))
     print('Treatment Effect: ATE: {ae: .4%}, ATT: {at:.4%}'.format(ae=exp_outcome / control_outcome - 1,
@@ -168,7 +290,13 @@ def main(file_path):
 
 
 if __name__ == '__main__':
-    inp = '../config/schema.yaml,../data/train.csv'
-    file_path = inp.split(",")
+    # data input path
+    input = '../config/schema.yaml,../data/train.csv'
+    # model config
+    model_config = {
+            'model_type': 'lgb',
+            'num_boost_round': 100
+        }
 
-    main(file_path)
+    # model training config
+    main(input, model_config)
